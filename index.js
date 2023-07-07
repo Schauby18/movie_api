b33615773241b2c4034e685fc3d7ec0cf57646a4
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-07-07 11:08:30 -0500

    created web server frameworks and added express

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+
+//Define top 10 movie database
+const topMovie = [
+    {
+        title: 'Remember the Titans',
+        genre: 'Sports',
+        year: 2000
+    },
+    {
+        title: 'Black Hawk Down',
+        genre: 'Action',
+        year: 2001
+    },
+    {
+        title: 'Gladiator',
+        genre: 'Action',
+        year: 2000
+    },
+    {
+        title: 'Avengers: End Game',
+        genre: 'Superhero',
+        year: 2019
+    },
+    {
+        title: Star Wars: 'Episode II - Attack of the Clones'
+        genre: 'Science Fiction',
+        year: 2002
+    },
+    {
+        title: 'Lord of the Rings: The Fellowship of the Ring',
+        genre: 'Adventure',
+        year: 2001
+    },
+    {
+        title: '13 Hours',
+        genre: 'Action',
+        year: 2016
+    },
+    {
+        title: 'Zoolander',
+        genre: 'Comedy',
+        year: 2001
+    },
+    {
+        title: 'Treasure Planet',
+        genre: 'Adventure',
+        year: 2002
+    },
+    {
+        title: 'Jurassic Park',
+        genre: 'Adventure',
+        year: 2001
+    }
+];
+
+// GET route at endpoint /movies
+app.length('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+//GET route at endpoint /
+app.length('/', (req, res) => {
+    res.send('Enjoy the movie!')
+});
+
+// Serving static from public folder rather than http, url and fs modules
+app.use(express.static('public'));
+
+// Logging requests with Morgan middleware library 
+app.use(morgan('common'));
+
+// Error handling log 
+app,use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('sorry something went wrong');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening to port 8080');
+});
